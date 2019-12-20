b57eeabcdf28ef27cc821038145823269d3397d9
alindobx <tonyromebx@gmail.com>
2019-12-19 22:58:55 -0500

    clock-app version 1.2

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,7 @@ function myTime(){
 const date = new Date();
 //hour
 const hour = date.getHours();
-//convert military time to standard time
+//convert military time to standard time function
 const convertTime = () => {
     if(hour > 12){
         return hour - 12;
@@ -12,8 +12,8 @@ const convertTime = () => {
         return hour
     }
 };
+//store conversion in a variable
 const standardTime = convertTime();
-
 //minute
 const minute = date.getMinutes();
 //seconds
@@ -24,9 +24,16 @@ const day = date.getDay();
 const dayNmb = date.getUTCDay();
 //Month
 const month = date.getMonth();
+//Month Array
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const match = months[month];
+// Days of the Week Array
+    const daysOfweek =['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const weekMatch = daysOfweek[day];
+
 //show time on HTML document
-document.getElementById('main').innerHTML = `${standardTime}:${minute}:${seconds}
-${day} - ${month} ${dayNmb}`;
+document.getElementById('main').innerHTML = `${standardTime}:${minute}:${seconds}<br/>
+${weekMatch} - ${match} ${dayNmb}`;
 }
 myTime();
 
