e20da5a886914a1ba42a48bd15a14ffeae879c37
alindobx <tonyromebx@gmail.com>
2019-12-22 21:51:48 -0500

    check-in

--- js/clock.js
+++ js/clock.js
@@ -1,27 +1,27 @@
 //Problem: Create a clock that displays
 function myTime(){
 //Date object
-const date = new Date();
+    const date = new Date();
 //hour
-const hour = date.getHours();
+    const hour = date.getHours();
 //seconds
-const seconds = date.getSeconds();
+    const seconds = date.getSeconds();
 
 //convert military time to standard time function
-const convertTime = () => {
-    if(hour >= 12){
-        return hour - 12;
-    }else if(hour === 0){
-        return 12;
-    }else{
-        return hour
-    }
-};
+    const convertTime = () => {
+        if(hour >= 12){
+            return hour - 12;
+        }else if(hour === 0){
+            return 12;
+        }else{
+            return hour
+        }
+    };
 // AM or PM function
-const amPm =()=> {
-    return hour >= 12 ?  "pm" : "am";
-};
-const nightOrDay = amPm();
+    const amPm =()=> {
+        return hour >= 12 ?  "pm" : "am";
+    };
+    const nightOrDay = amPm();
 
 //add '0' to seconds first integer
     const newSecond = () => {
@@ -35,29 +35,28 @@ const nightOrDay = amPm();
 
 
 //store conversion in a variable
-const standardTime = convertTime();
+    const standardTime = convertTime();
 //minute
-const minute = date.getMinutes();
+    const minute = date.getMinutes();
 
 //Day
-const day = date.getDay();
+    const day = date.getDay();
 //Integer of Day
-const dayNmb = date.getDate();
+    const dayNmb = date.getDate();
 //Month
-const month = date.getMonth();
+    const month = date.getMonth();
 //Month Array
     const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     const match = months[month];
 // Days of the Week Array
-    const daysOfweek =['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const weekMatch = daysOfweek[(day) - 1];
+    const daysOfweek =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const weekMatch = daysOfweek[day];
 
 //show time on HTML document
-document.getElementById('main').innerHTML = `${standardTime}:${minute}:${firstDigit}${seconds}${nightOrDay}<br/>
+    document.getElementById('main').innerHTML = `${standardTime}:${minute}:${firstDigit}${seconds}${nightOrDay}<br/>
 ${weekMatch} - ${match} ${dayNmb}`;
 }
 setInterval(myTime, 1000);
-
 
 
 
